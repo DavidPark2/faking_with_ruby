7d28e872c38e01a3de4605fe6634c01d53c00ce2
David Park <dpark543107@gmail.com>
2016-03-16 16:07:04 -0500

    practicing ruby class

---
+++ movie.rb
@@ -0,0 +1,31 @@
+class Movie
+  #attr_accessor
+  # 1. have a constructor that accepts a URL for OMDB
+  # 2. The constructor should then use HTTParty.get to get the data
+  # 3. You will then SET instance variables for @title @director @plot
+  # 4. Implement a to_s, to_hash, to_json
+  # 5. self.method_name == this.methodName() in JS
+  require 'HTTParty'
+  require 'json'
+
+  interstellar = HTTParty.get('http://www.omdbapi.com/?t=interstellar&y=&plot=short&r=json')
+
+  def to_s
+    self.to_s
+  end
+
+  def to_hash
+    return {
+      :title => @title,
+      :director => @director,
+      :email => @email
+    }
+  end
+
+  def to_json
+    self.to_hash.to_json
+  end
+
+end
+
+movies = Movie.new()
